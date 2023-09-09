51d9f122b51b23d69669674a08689664636f69f9
Manfred Touron <94029+moul@users.noreply.github.com>
2023-09-09 15:35:43 +0200

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,67 @@
+const profiles = {
+  "Manfred": {
+    birthDate: "1988-01-25",
+    title: "Manfred’s Life in Weeks",
+    yearsToShow: "90",
+    eventsData: {
+      events: [
+      ]
+    },
+  },
+  "Einstein": {
+    birthDate: "1879-03-14",
+    title: "Einstein’s Life in Weeks",
+    yearsToShow: "76",
+    eventsData: {
+      events: [
+        { date: "1905-01-01", description: "Annus Mirabilis papers", color: "#D4AF37" },
+        { date: "1915-11-25", description: "Presented the field equations of general relativity", color: "#C0C0C0" },
+      ]
+    },
+  },
+  "Gandhi": {
+    birthDate: "1869-10-02",
+    title: "Gandhi’s Life in Weeks",
+    yearsToShow: "79",
+    eventsData: {
+      events: [
+        { date: "1930-03-12", description: "Start of Dandi March", color: "#D4AF37" },
+        { date: "1942-08-08", description: "Launch of Quit India Movement", color: "#C0C0C0" },
+      ]
+    },
+  },
+  "Jesus": {
+    birthDate: "0000-12-25", // Symbolic date
+    title: "Jesus’ Life in Weeks",
+    yearsToShow: "33",
+    eventsData: {
+      events: [
+        { date: "0030-04-05", description: "Crucifixion (approximated)", color: "#D4AF37" },
+      ]
+    },
+  },
+  "Elon Musk": {
+    birthDate: "1971-06-28",
+    title: "Elon Musk’s Life in Weeks",
+    yearsToShow: "100", // Projected for a full lifespan, can be adjusted.
+    eventsData: {
+      events: [
+        { date: "2002-03-14", description: "Founded SpaceX", color: "#D4AF37" },
+        { date: "2004-02-01", description: "Joined Tesla Motors", color: "#C0C0C0" },
+        // ... and other significant events
+      ]
+    },
+  },
+  "Steve Jobs": {
+    birthDate: "1955-02-24",
+    title: "Steve Jobs’ Life in Weeks",
+    yearsToShow: "56",
+    eventsData: {
+      events: [
+        { date: "1976-04-01", description: "Founded Apple with Wozniak", color: "#D4AF37" },
+        { date: "2007-01-09", description: "Introduced the iPhone", color: "#C0C0C0" },
+        // ... and other significant events
+      ]
+    },
+  },
+};
